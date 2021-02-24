4e2fe42703f3afce10b9888412d6d9e7dd5dfd87
Ayrton Souza <ayrtonsouza@icloud.com>
2021-02-24 17:07:25 -0300

    developing image label recognition and translation

--- handler.js
+++ handler.js
@@ -1,11 +1,37 @@
 'use strict';
+const { promises: { readFile } } = require('fs')
 class Handler {
-  constructor({ rekognitionService }) {
+  constructor({ rekognitionService, translatorService }) {
     this.rekognitionService = rekognitionService
+    this.translatorService = translatorService
+  }
+
+  async detectImageLabels(buffer) {
+    const result = await this.rekognitionService.detectLabels({
+      Image: {
+        Bytes: buffer
+      }
+    }).promise()
+    const workingItems = result.Labels.filter(({ Confidence }) => Confidence > 80)
+    const names = workingItems.map(({ Name }) => Name).join(', ')
+    return { names, workingItems }
+  }
+
+  async translateText(text) {
+    const params = {
+      SourceLanguageCode: 'en',
+      TargetLanguageCode: 'pt',
+      Text: text
+    }
+    const result = await this.translatorService.translateText(params).promise()
+    console.log(JSON.stringify(result))
   }
 
   async main(event) {
     try {
+      const imageBuffer = await readFile('./images/cat.png')
+      const { names, workingItems } = await this.detectImageLabels(imageBuffer)
+      const texts = await this.translateText(names)
       return {
         statusCode: 200,
         body: 'Hello!'
@@ -22,7 +48,9 @@ class Handler {
 // Factory:
 const aws = require('aws-sdk')
 const rekognition = new aws.Rekognition()
+const translator = new aws.Translate()
 const handler = new Handler({
-  rekognitionService = rekognition
+  rekognitionService: rekognition,
+  translatorService: translator
 })
 module.exports.main = handler.main.bind(handler)
